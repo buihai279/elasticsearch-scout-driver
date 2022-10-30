0c2d8a7a518d02b0ca70d273702e516824baef3a
Hoang Pham <hoangmaths96@gmail.com>
2022-10-30 22:18:25 +0700

    wip

--- src/ExplorerServiceProvider.php
+++ src/ExplorerServiceProvider.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace JeroenG\Explorer;
 
-use Elastic\Elasticsearch\ClientBuilder;
+use Elastic\Elasticsearch\Client;
 use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Support\ServiceProvider;
 use JeroenG\Explorer\Application\DocumentAdapterInterface;
@@ -13,8 +13,8 @@ use JeroenG\Explorer\Domain\Aggregations\AggregationSyntaxInterface;
 use JeroenG\Explorer\Domain\IndexManagement\IndexConfigurationRepositoryInterface;
 use JeroenG\Explorer\Infrastructure\Console\ElasticSearch;
 use JeroenG\Explorer\Infrastructure\Console\ElasticUpdate;
-use JeroenG\Explorer\Infrastructure\Elastic\ElasticClientFactory;
 use JeroenG\Explorer\Infrastructure\Elastic\ElasticClientBuilder;
+use JeroenG\Explorer\Infrastructure\Elastic\ElasticClientFactory;
 use JeroenG\Explorer\Infrastructure\Elastic\ElasticDocumentAdapter;
 use JeroenG\Explorer\Infrastructure\Elastic\ElasticIndexAdapter;
 use JeroenG\Explorer\Infrastructure\IndexManagement\ElasticIndexConfigurationRepository;
@@ -29,83 +29,82 @@ class ExplorerServiceProvider extends ServiceProvider
         if ($this->app->runningInConsole()) {
             $this->bootForConsole();
         }
-
-        $this->app->bind(ElasticClientFactory::class, function () {
-            return ElasticClientBuilder::fromConfig(config())->build();
-        });
-
-        $this->app->bind(IndexAdapterInterface::class, ElasticIndexAdapter::class);
-
-        $this->app->bind(DocumentAdapterInterface::class, ElasticDocumentAdapter::class);
-
-        $this->app->bind(IndexConfigurationRepositoryInterface::class, function () {
-            return new ElasticIndexConfigurationRepository(
+        
+        $this->app->when(ElasticClientFactory::class)
+            ->needs(Client::class)
+            ->give(fn() => ElasticClientBuilder::fromConfig(config())->build());
+        
+        $this->app->singleton(IndexAdapterInterface::class, ElasticIndexAdapter::class);
+        
+        $this->app->singleton(DocumentAdapterInterface::class, ElasticDocumentAdapter::class);
+        
+        $this->app->singleton(
+            IndexConfigurationRepositoryInterface::class,
+            fn() => new ElasticIndexConfigurationRepository(
                 config('explorer.indexes') ?? [],
                 config('explorer.prune_old_aliases'),
-            );
-        });
-
-        resolve(EngineManager::class)->extend('elastic', function (Application $app) {
-            return new ElasticEngine(
-                $app->make(IndexAdapterInterface::class),
-                $app->make(DocumentAdapterInterface::class),
-                $app->make(IndexConfigurationRepositoryInterface::class)
-            );
-        });
-
+            )
+        );
+        
+        resolve(EngineManager::class)->extend('elastic', fn(Application $app) => new ElasticEngine(
+            $app->make(IndexAdapterInterface::class),
+            $app->make(DocumentAdapterInterface::class),
+            $app->make(IndexConfigurationRepositoryInterface::class)
+        ));
+        
         Builder::macro('must', function ($must) {
             $this->must[] = $must;
             return $this;
         });
-
+        
         Builder::macro('should', function ($should) {
             $this->should[] = $should;
             return $this;
         });
-
+        
         Builder::macro('filter', function ($filter) {
             $this->filter[] = $filter;
             return $this;
         });
-
+        
         Builder::macro('field', function (string $field) {
             $this->fields[] = $field;
             return $this;
         });
-
+        
         Builder::macro('fields', function (array $fields) {
             $this->fields = $fields;
-
+            
             return $this;
         });
-
+        
         Builder::macro('newCompound', function ($compound) {
             $this->compound = $compound;
             return $this;
         });
-
+        
         Builder::macro('aggregation', function (string $name, AggregationSyntaxInterface $aggregation) {
             $this->aggregations[$name] = $aggregation;
             return $this;
         });
     }
-
+    
     public function register(): void
     {
         $this->mergeConfigFrom(__DIR__ . '/../config/explorer.php', 'explorer');
     }
-
+    
     public function provides(): array
     {
         return ['explorer'];
     }
-
+    
     protected function bootForConsole(): void
     {
         $this->publishes([
             __DIR__ . '/../config/explorer.php' => config_path('explorer.php'),
         ], 'explorer.config');
-
+        
         $this->commands([
             ElasticSearch::class,
             ElasticUpdate::class,
--- src/Infrastructure/Elastic/ElasticClientBuilder.php
+++ src/Infrastructure/Elastic/ElasticClientBuilder.php
@@ -10,60 +10,63 @@ use Illuminate\Contracts\Config\Repository;
 final class ElasticClientBuilder
 {
     private const HOST_KEYS = ['host', 'port', 'scheme'];
-
+    
     public static function fromConfig(Repository $config): ClientBuilder
     {
         $builder = ClientBuilder::create();
-
+        
         $hostConnectionProperties = array_filter(
             $config->get('explorer.connection'),
-            static fn ($key) => in_array($key, self::HOST_KEYS, true),
+            static fn($key) => in_array($key, self::HOST_KEYS, true),
             ARRAY_FILTER_USE_KEY
         );
-
-        $builder->setHosts($hostConnectionProperties);
-
+        
+        $builder->setHosts(
+            [$hostConnectionProperties['scheme'] . '://' . $hostConnectionProperties['host'] . ':' . $hostConnectionProperties['port']]
+        );
+        
         if ($config->has('explorer.additionalConnections')) {
-            $builder->setHosts([$config->get('explorer.connection'), ...$config->get('explorer.additionalConnections')]);
+            $builder->setHosts([$config->get('explorer.connection'), ...$config->get('explorer.additionalConnections')]
+            );
         }
-
-        if($config->has('explorer.api')) {
+        
+        if ($config->has('explorer.api')) {
             $builder->setApiKey(
                 $config->get('explorer.api.id'),
                 $config->get('explorer.api.key')
             );
         }
-
-        if($config->has('explorer.elasticCloudId')) {
+        
+        if ($config->has('explorer.elasticCloudId')) {
             $builder->setElasticCloudId(
                 $config->get('explorer.elasticCloudId'),
             );
         }
-
-        if($config->has('explorer.auth')) {
+        
+        if ($config->has('explorer.auth')) {
             $builder->setBasicAuthentication(
                 $config->get('explorer.auth.username'),
                 $config->get('explorer.auth.password')
             );
         }
-
-        if($config->has('explorer.ssl.verify')) {
-            $builder->setSSLVerification($config->get('explorer.ssl.verify'));
+        
+        if ($config->has('explorer.ssl_verification')) {
+            $builder->setSSLVerification($config->get('explorer.ssl_verification'));
         }
-
-        if($config->has('explorer.ssl.key')) {
+        
+        if ($config->has('explorer.ssl.key')) {
             [$path, $password] = self::getPathAndPassword($config->get('explorer.ssl.key'));
             $builder->setSSLKey($path, $password);
         }
-
-        if($config->has('explorer.ssl.cert')) {
+        
+        if ($config->has('explorer.ssl.cert')) {
             [$path, $password] = self::getPathAndPassword($config->get('explorer.ssl.cert'));
             $builder->setSSLCert($path, $password);
         }
-
+        
         return $builder;
     }
-
+    
     /**
      * @param array|string $config
      */
